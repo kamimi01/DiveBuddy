d76ceaf0298565ef1ffd2ecfef979d366c58173c
mika.urakawa <awakaru0122@gmail.com>
2024-08-03 12:25:27 -0700

    feat: initial a Xcode project

---
+++ DiveBuddy/DiveBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiveBuddyApp.swift
+//  DiveBuddy
+//
+//  Created by mikaurakawa on 2024-08-03.
+//
+
+import SwiftUI
+
+@main
+struct DiveBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

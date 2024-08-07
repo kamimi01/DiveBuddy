1c43cb23f775edc81ec3d16c1be97c2f78740806
mika.urakawa <awakaru0122@gmail.com>
2024-08-07 10:30:43 -0700

    feat: sign up screen

--- DiveBuddy/Auth/SignupView.swift
+++ DiveBuddy/Auth/SignupView.swift
@@ -8,8 +8,90 @@
 import SwiftUI
 
 struct SignupView: View {
+    @ObservedObject private var viewModel = SignupViewModel()
+
     var body: some View {
-        Text("supposed to be sign up screen")
+        VStack(alignment: .leading, spacing: 30) {
+            signupText()
+                .padding(.top, 40)
+            emailPassInput()
+                .padding(.top, 20)
+            signupButton()
+            Spacer()
+            thirdPartyLoginView()
+        }
+        .padding(.horizontal, 20)
+    }
+}
+
+private extension SignupView {
+    func signupText() -> some View {
+        Text("Sign up")
+            .font(.customFont(size: .one, weight: .bold))
+    }
+
+    func emailPassInput() -> some View {
+        VStack(spacing: 20) {
+            VStack(alignment: .leading, spacing: 6) {
+                Text("Your email")
+                    .font(.customFont(size: .three))
+                TextField("", text: $viewModel.emailInput)
+                    .roundedTextField()
+            }
+            VStack(alignment: .leading, spacing: 6) {
+                Text("Password")
+                    .font(.customFont(size: .three))
+                SecureField("", text: $viewModel.passwordInput)
+                    .roundedTextField()
+            }
+        }
+    }
+
+    func signupButton() -> some View {
+        VStack(spacing: 14) {
+            Button(action: {}) {
+                Text("Sign up")
+                    .font(.customFont(size: .three))
+                    .foregroundStyle(.primaryWhite)
+                    .padding(.vertical, 11)
+                    .frame(maxWidth: .infinity)
+            }
+            .roundedButton(.three)
+            HStack(spacing: 20) {
+                Text("Do you have account?")
+                    .font(.customFont(size: .two))
+                    .foregroundStyle(.primaryTextBlack)
+                Button(action: {
+                    viewModel.didTapLoginButton()
+                }) {
+                    Text("Log in")
+                        .font(.customFont(size: .two, weight: .bold))
+                        .foregroundStyle(.accentBlue)
+                }
+                .fullScreenCover(isPresented: $viewModel.isPresentedLoginView) {
+                    LoginView()
+                }
+            }
+        }
+    }
+
+    func thirdPartyLoginView() -> some View {
+        VStack(spacing: 20) {
+            Text("Or sign up with")
+                .font(.customFont(size: .three))
+                .foregroundStyle(.primaryTextBlack)
+                .frame(maxWidth: .infinity)
+            // TODO: replace with FirebaseUI
+            Button(action: {}) {
+                Text("ThirdParyLogin: Google (will be replaced with FirebaseUI)")
+            }
+            Button(action: {}) {
+                Text("ThirdParyLogin: Facebook (will be replaced with FirebaseUI)")
+            }
+            Button(action: {}) {
+                Text("ThirdParyLogin: X (will be replaced with FirebaseUI)")
+            }
+        }
     }
 }
 
---
+++ DiveBuddy/Auth/SignupViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  SignupViewModel.swift
+//  DiveBuddy
+//
+//  Created by mikaurakawa on 2024-08-07.
+//
+
+import Foundation
+
+final class SignupViewModel: ObservableObject {
+    @Published var isPresentedLoginView = false
+    @Published var emailInput = ""
+    @Published var passwordInput = ""
+
+    func didTapLoginButton() {
+        isPresentedLoginView = true
+    }
+}

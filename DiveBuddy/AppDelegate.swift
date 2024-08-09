44aabfdced3371c879ddd992f754ec92d889eeb9
mika.urakawa <awakaru0122@gmail.com>
2024-08-09 16:47:10 -0700

    feat: init firebase

---
+++ DiveBuddy/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  DiveBuddy
+//
+//  Created by mikaurakawa on 2024-08-09.
+//
+
+import UIKit
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
--- DiveBuddy/DiveBuddyApp.swift
+++ DiveBuddy/DiveBuddyApp.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 @main
 struct DiveBuddyApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             WelcomeView()

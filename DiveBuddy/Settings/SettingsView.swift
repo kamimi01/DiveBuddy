dea233a80120c3041c26d928a9210c64aaed3623
mika.urakawa <awakaru0122@gmail.com>
2024-08-09 11:55:12 -0700

    feat: settings view

--- DiveBuddy/Settings/SettingsView.swift
+++ DiveBuddy/Settings/SettingsView.swift
@@ -8,8 +8,97 @@
 import SwiftUI
 
 struct SettingsView: View {
+    @Environment(\.openURL) var openURL
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            profile()
+                .padding(.horizontal, 20)
+            List {
+                Section(header: Text("Support")) {
+                    knowDeveloper()
+                }
+
+                Section(header: Text("About App")) {
+                    termsOfUse()
+                    privacyPolicy()
+                    version()
+                }
+            }
+            .listStyle(.plain)
+            .frame(maxHeight: 300)
+            logoutButton()
+            Spacer()
+        }
+        .padding(.top, 20)
+    }
+}
+
+private extension SettingsView {
+    func profile() -> some View {
+        Text("Guest")
+            .foregroundStyle(.primaryTextBlack)
+            .font(.customFont(size: .five, weight: .bold))
+    }
+
+    func termsOfUse() -> some View {
+        Button(action:{
+            openURL(URL(string: "https://github.com/kamimi01")!)
+        }) {
+            HStack {
+                Text("Terms of User")
+                Spacer()
+            }
+        }
+        .foregroundStyle(.primaryTextBlack)
+    }
+
+    func privacyPolicy() -> some View {
+        Button(action:{
+            openURL(URL(string: "https://github.com/kamimi01")!)
+        }) {
+            HStack {
+                Text("Privacy Policy")
+                Spacer()
+            }
+        }
+        .foregroundStyle(.primaryTextBlack)
+    }
+
+    func knowDeveloper() -> some View {
+        Button(action:{
+            openURL(URL(string: "https://github.com/kamimi01")!)
+        }) {
+            HStack {
+                Text("Know Developer")
+                Spacer()
+            }
+        }
+        .foregroundStyle(.primaryTextBlack)
+    }
+
+    func version() -> some View {
+        let version = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String ?? ""
+        let buildNumber = Bundle.main.object(forInfoDictionaryKey: "CFBundleVersion") as? String ?? ""
+
+        return HStack {
+            Text("Version")
+                .foregroundStyle(.primaryTextBlack)
+            Spacer()
+            Text("\(version)(\(buildNumber))")
+                .foregroundStyle(.primaryTextBlack)
+        }
+    }
+
+    func logoutButton() -> some View {
+        Button(action: {}) {
+            Text("Logout")
+                .font(.customFont(size: .three, weight: .bold))
+                .foregroundStyle(.accentBlue)
+                .padding(.vertical, 11)
+                .frame(maxWidth: .infinity)
+        }
+        .roundedButton(.one)
     }
 }
 

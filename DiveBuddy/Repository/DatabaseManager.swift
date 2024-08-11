d29d90217352d7450a40d168eb6dc62e31530e62
mika.urakawa <awakaru0122@gmail.com>
2024-08-11 15:13:49 -0700

    feat: upload an image to cloud storage

--- DiveBuddy/Repository/DatabaseManager.swift
+++ DiveBuddy/Repository/DatabaseManager.swift
@@ -7,6 +7,8 @@
 
 import Foundation
 import FirebaseDatabase
+import FirebaseStorage
+import UIKit
 
 final class DatabaseManager {
     private var ref: DatabaseReference!
@@ -17,37 +19,62 @@ final class DatabaseManager {
 
     /// Create gear data
     func create(uid: String, gear: Gear) async {
-        // TODO: Upload gear image to cloud storage
-
-        // TODO: Create new data in firebase realtime database
         do {
-            print("Firebase write started")
-            var maitenanceHistories: [String: Bool] {
-                var histories: [String: Bool] = [:]
-                for maintenance in gear.maintenanceHistories {
-                    histories[maintenance.id] = true
-                }
-                return histories
+            let data = try await createGearData(uid: uid, gear: gear)
+
+            guard let key = data?.key else {
+                print("cannot find a key")
+                return
             }
-            let gear: [String : Any] = [
-                "userID": uid,
-                "name": gear.name,
-                "imageURL": "url in cloud storage",  // TODO: replace url after implementing cloud storage
-                "brandName": gear.brandName,
-                "currency": gear.currency.rawValue,
-                "price": gear.price,
-                "maintenanceHistories": maitenanceHistories,
-                "_updatedAt": Int(Date().timeIntervalSince1970),
-                "_createdAt": Int(Date().timeIntervalSince1970)
-            ]
-            try await ref?.child("Gear").childByAutoId().updateChildValues(gear)
+            await uploadImage(uid: uid, gearID: key, data: gear.imageData)
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
 
-            print("Firebase write finished")
+    /// Create new data in firebase realtime database
+    private func createGearData(uid: String, gear: Gear) async throws -> DatabaseReference? {
+        print("Firebase write started")
+        var maitenanceHistories: [String: Bool] {
+            var histories: [String: Bool] = [:]
+            for maintenance in gear.maintenanceHistories {
+                histories[maintenance.id] = true
+            }
+            return histories
+        }
+        let newGear: [String : Any] = [
+            "userID": uid,
+            "name": gear.name,
+            "imageURL": "url in cloud storage",  // TODO: replace url after implementing cloud storage
+            "brandName": gear.brandName,
+            "currency": gear.currency.rawValue,
+            "price": gear.price,
+            "maintenanceHistories": maitenanceHistories,
+            "_updatedAt": Int(Date().timeIntervalSince1970),
+            "_createdAt": Int(Date().timeIntervalSince1970)
+        ]
+        let data = try await ref?.child("Gear").childByAutoId().updateChildValues(newGear)
+        print("data:", data)
+        print("Firebase write finished")
+        
+        return data
 
-            print("Firebase read started")
-            let snapshot = try await ref?.child("Gear").getData()
-            print("received data:", snapshot!)
-            print("Firebase read finished")
+//        print("Firebase read started")
+//        let snapshot = try await ref?.child("Gear").getData()
+//        print("received data:", snapshot!)
+//        print("Firebase read finished")
+    }
+
+    /// Upload gear image to cloud storage
+    private func uploadImage(uid: String, gearID: String, data: Data) async {
+        let storage = Storage.storage()
+        let storageRef = storage.reference()
+        let imagesRef = storageRef.child("gears/\(uid)/\(gearID).png")
+
+        do {
+            _ = try await imagesRef.putDataAsync(data)
+            let downloadURL = try await imagesRef.downloadURL()
+            print("download URL:", downloadURL)
         } catch {
             print(error.localizedDescription)
         }
